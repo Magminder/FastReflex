4f27eef79878594112890f18f0f45175100e8ee1
Alex Manko <magminder@gmail.com>
2015-10-19 00:20:09 +0300

    Added ability to create test pages. Added webserver browser-sync. Changed gulp deprecated commands Reduced lint errors count

--- gulpfile.js
+++ gulpfile.js
@@ -2,45 +2,71 @@
  * Created by Alex Manko on 17.10.2015.
  */
 
-var gulp = require('gulp');
-var browserify = require('browserify');
-var source = require('vinyl-source-stream');
-var refresh = require('gulp-livereload');
-var jshint = require('gulp-jshint');
-var rename = require('gulp-rename');
-var uglify = require('gulp-uglify');
-var streamify = require('gulp-streamify');
+var gulp = require('gulp'),
+    browserify = require('browserify'),
+    source = require('vinyl-source-stream'),
+    jshint = require('gulp-jshint'),
+    rename = require('gulp-rename'),
+    uglify = require('gulp-uglify'),
+    streamify = require('gulp-streamify'),
+    browserSync = require('browser-sync'),
+    insert = require('gulp-insert'),
+    reload = browserSync.reload;
 
-var lr = require('tiny-lr');
-var server = lr();
+var testServerConfig = {
+    server: {
+        baseDir: './test'
+    },
+    tunnel: true,
+    host: 'localhost',
+    port: 9000,
+    logPrefix: "Test"
+};
 
-gulp.task('lint', function() {
+var src = function() {
     return browserify('./src/lib.js')
         .bundle()
         .pipe(source('lib.js'))
+        .pipe(insert.transform(function(contents, file) {
+            return contents
+                .replace('\n', '\n/* jshint ignore:end */\n', 1);
+        }))
+        .pipe(insert.prepend('/* jshint ignore:start */\n'));
+};
+
+gulp.task('lint', function() {
+    return src()
         .pipe(streamify(jshint()))
         .pipe(jshint.reporter('default'));
 });
 
 gulp.task('minify', function() {
-    return browserify('./src/lib.js')
-        .bundle()
-        .pipe(source('lib.js'))
+    return src()
         .pipe(gulp.dest('./build'))
         .pipe(streamify(uglify()))
         .pipe(rename('lib.min.js'))
         .pipe(gulp.dest('./build'))
-        .pipe(refresh(server))
+        .pipe(reload({stream: true}));
 });
 
-gulp.task('lr-server', function() {
-    server.listen(3000, function(err) {
-        if(err) return console.log(err);
+gulp.task('test', function() {
+    browserSync(testServerConfig);
+});
+
+gulp.task('watch', function(){
+    watch('src/**', ['lint', 'minify']);
+
+    watch('test/**', function() {
+        reload({stream: true});
     });
 });
 
 gulp.task('default', function() {
-    gulp.run('lr-server', 'lint', 'minify');
+    gulp.start('lint', 'minify', 'test');
 
     gulp.watch('src/**', ['lint', 'minify']);
+
+    gulp.watch('test/**', function() {
+        reload();
+    });
 });
